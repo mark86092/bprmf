dac2042fccb828d8199260838ffa1df70a70c2ea
Yen-chi Chen <mark86092@gmail.com>
2014-11-15 20:16:46 +0800

    update

--- bprmf.cpp
+++ bprmf.cpp
@@ -8,6 +8,10 @@
 
 using namespace std;
 
+bool sort_by_y(const Pair_i_yui &i, const Pair_i_yui &j) {
+	return (i.y > j.y);
+}
+
 int main(int argc, const char* argv[])
 {
     /** preprocessing **/
@@ -26,6 +30,28 @@ int main(int argc, const char* argv[])
 
     /** model declaration **/
     Model model(train.Nuser(), train.Nitem(), K);
+	
+	//vector<Pair> sample = train.draw_sample(1);
 
-    /** core part **/
+	/* caculate score of u */
+	fstream f;
+	f.open("../test_nodes.txt", ios::in);
+	size_t u;
+	f >> u;
+
+	vector<Pair_i_yui> predict;
+	for (size_t i = 0; i < train.Nitem(); i++) {
+		if (i == u)
+			continue;
+		if (train.i_set[u].find(i) != train.i_set[u].end())
+			continue;
+		double y = model.P.row(u).dot(model.Q.row(i));
+		predict.push_back(Pair_i_yui(i, y));
+	}
+	
+	sort(predict.begin(), predict.end(), sort_by_y);
+    for (size_t i = 0; i < 30; i++) {
+		cout << predict[i].i << " " << predict[i].y << endl;
+	}
+	/** core part **/
 }
