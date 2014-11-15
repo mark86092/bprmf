88b8f65bd4483977c645daf0182e2dde1c2aee6f
Yen-chi Chen <mark86092@gmail.com>
2014-11-15 23:31:21 +0800

    run bprmf

--- bprmf.cpp
+++ bprmf.cpp
@@ -11,44 +11,50 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    /** preprocessing **/
-    srand( 0 );
+	/** preprocessing **/
+	srand( 0 );
 
 	const char train_file[] = "../train_edges.bpr";
 	const int K = 10;
-	const double learning_rate = 0.05;
+	const double learning_rate = 0.1;
 	const double reg_user = 0.01;
 	const double reg_item = 0.01;
 
-    /** data part **/
-    Train train;
-    train.Read_File(train_file);
-    cerr << train.Nuser() << " " << train.Nitem() << endl;
+	/** data part **/
+	Train train;
+	train.Read_File(train_file);
+	cerr << train.Nuser() << " " << train.Nitem() << endl;
 
-    /** model declaration **/
-    Model model(train.Nuser(), train.Nitem(), K);
-	
-    for (int iter = 0; iter < 100; iter++) {
-        cout << iter << endl;
-    	vector<Pair> sample = train.draw_sample(1);
-        model.update(sample, learning_rate, reg_user, reg_item);
-    }
+	/** model declaration **/
+	Model model(train.Nuser(), train.Nitem(), K);
 
-	/* caculate score of u */
-	fstream f;
-	f.open("../test_nodes.txt", ios::in);
-    fstream p; 
-    p.open("predict.txt", ios::out);
-	size_t u;
-	while (f >> u) 
-    {
-        p << u << ":";
-        vector<Pair_i_y> predict = predict_top_N(train, model, u, 30);
-        p << predict[0].i;
-        for (size_t n = 1; n < 30; n++)
-        {
-            p << "," << predict[n].i;
-        }
-        p << endl;
-    }
+	for (int iter = 0; iter < 1000; iter++) {
+		cout << iter << endl;
+		vector<Pair> sample = train.draw_sample(1);
+		model.update(sample, learning_rate, reg_user, reg_item);
+
+		if (iter % 100 == 99) {
+			fstream f;
+			f.open("../test_nodes.txt", ios::in);
+			fstream p; 
+			char filename[100];
+			sprintf(filename, "predict_%d.txt", iter);
+			p.open(filename, ios::out);
+			size_t u;
+			while (f >> u) 
+			{
+				p << u << ":";
+				vector<Pair_i_y> predict = predict_top_N(train, model, u, 30);
+				p << predict[0].i;
+				for (size_t n = 1; n < 30; n++)
+				{
+					p << "," << predict[n].i;
+				}
+				p << endl;
+			}
+		}
+
+
+	}
+
 }
